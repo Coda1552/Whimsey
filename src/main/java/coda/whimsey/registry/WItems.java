da1781389ee88145c6a53aef1a9b54c6740bf933
Coda <cody.koelliker@outlook.com>
2023-02-06 15:34:38 -0700

    start ashensteel stuff but without textures because icy didnt send them yet

--- src/main/java/coda/whimsey/registry/WItems.java
+++ src/main/java/coda/whimsey/registry/WItems.java
@@ -13,4 +13,6 @@ public class WItems {
     public static final RegistryObject<Item> PHOENIX_ASHES = ITEMS.register("phoenix_ashes", () -> new Item(new Item.Properties().tab(Whimsey.GROUP)));
 
     public static final RegistryObject<Item> PHOENIX_SPAWN_EGG = ITEMS.register("phoenix_spawn_egg", () -> new ForgeSpawnEggItem(WEntities.PHOENIX, 0xf07a08, 0x9e472e, new Item.Properties().tab(Whimsey.GROUP)));
+
+    public static final RegistryObject<Item> ASHENSTEEL_INGOT = ITEMS.register("ashensteel_ingot", () -> new Item(new Item.Properties().tab(Whimsey.GROUP).fireResistant()));
 }
